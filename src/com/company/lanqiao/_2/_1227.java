f019f7e910d13c80691bd044063cf314547922f5
xie-super <2507062649@qq.com>
2022-03-02 18:41:25 +0800

    second commit

--- src/com/company/lanqiao/_2/_1227.java
+++ src/com/company/lanqiao/_2/_1227.java
@@ -1,9 +1,54 @@
 package com.company.lanqiao._2;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * Author:xie-super
  * Time:2022/3/1
  * Name:1227. 分巧克力
  */
 public class _1227 {
+    private static final int N = 100010;
+    static int[] h = new int[N];
+    static int[] w = new int[N];
+    static int n,k;
+    public static boolean check(int l){
+        int num = 0;
+        for(int i = 0;i<n;i++){
+            num+=(h[i]/l)*(w[i]/l);
+            if(num>=k){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+         n = scanner.nextInt();
+         k = scanner.nextInt();
+
+        for(int i = 0; i<n ;i++){
+
+            h[i]= scanner.nextInt();
+            w[i] = scanner.nextInt();
+
+        }
+        int r = 100010, l = 1;
+        int result = 0;
+       while(l<r){
+           int mid = l + (r-l+1>>1);
+           if(check(mid)){
+               l = mid;
+           }else {
+               r = mid -1;
+           }
+
+       }
+        System.out.println(r);
+
+
+
+
+    }
 }
---
+++ src/com/company/lanqiao/_2/_730.java
@@ -0,0 +1,51 @@
+package com.company.lanqiao._2;
+
+import java.util.Scanner;
+
+/**
+ * Author:xie-super
+ * Time:2022/3/2
+ * Name:730. 机器人跳跃问题
+ *
+ */
+public class _730 {
+    private static final int N = 100010;
+    static int [] array = new int[N];
+    static int n;
+
+    static boolean check(int num){
+        for(int i  = 0 ;i<n; i++){
+            if((2*num - array[i])>=0){
+                num = 2*num - array[i];
+                //越界问题，当num足够大时会成为负数
+                if(num >=1e5) return true;
+            }else {
+                return false;
+            }
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        for(int i = 0; i<n;i++){
+            array[i] = input.nextInt();
+        }
+
+        int l  =0, r = 100000;
+        while(l<r){
+            int mid  = l + (r-l+1>>1);
+
+            if(check(mid)){
+                System.out.println(l+" "+mid);
+                r  = mid;
+            }else {
+                l = mid+1;
+            }
+        }
+        System.out.println(r);
+
+    }
+}
